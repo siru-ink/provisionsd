a0d397cb8d850a12671c63a13127cbd68c014527
siru <email@siru.ink>
2026-03-21 20:52:14 +0100

    WIP on database model

--- src/db.rs
+++ src/db.rs
@@ -55,6 +55,34 @@ pub fn database_setup() -> AdHoc {
         .await
         .expect("Database table meal should exist or be creatable.");
 
+        pool.execute(
+            "
+            create table if not exists 'units' (
+            'id' integer not null unique,
+            'name' text not null unique,
+            'abbr' text not null unique,
+            primary key('id' autoincrement)
+            );",
+        )
+        .await
+        .expect("Database table units should exist or be creatable.");
+
+        pool.execute(
+            "
+            create table if not exists 'meal_ingredients' (
+            'mealid' integer not null,
+            'ingredientid' integer not null,
+            'quantity' real not null,
+            'unit' id not null,
+            foreign key('mealid') references 'meal'('id') on delete cascade,
+            foreign key('ingredientid') references 'ingredients'('id') on delete cascade,
+            foreign key('unit') references 'units'('id') on delete cascade,
+            primary key('meal_id', 'ingredient_id')
+            );",
+        )
+        .await
+        .expect("Database table meal_ingredients should exist or be creatable.");
+
         Ok(rocket)
     })
 }
