1c79b8cc75d2cad170236a11307912c6f644b460
siru <email@siru.ink>
2026-04-04 16:38:17 +0200

    Altered login post func to use closure for db access

--- cmd/runserver/main.go
+++ cmd/runserver/main.go
@@ -60,7 +60,7 @@ func main() {
 
 	loginRouter := r.PathPrefix("/auth").Subrouter()
 	loginRouter.HandleFunc("/", auth(authIndexRoute))
-	loginRouter.HandleFunc("/login/", authLoginPost).Methods("POST")
+	loginRouter.HandleFunc("/login/", loginPost(defaultDB)).Methods("POST")
 	loginRouter.HandleFunc("/login/", loginGet).Methods("GET")
 	loginRouter.HandleFunc("/logout/", logoutRoute).Methods("POST")
 
@@ -113,34 +113,42 @@ func loginGet(w http.ResponseWriter, r *http.Request) {
 	templ.Execute(w, "")
 }
 
-func authLoginPost(w http.ResponseWriter, r *http.Request) {
-	uname := r.FormValue("uname")
-	passwd := r.FormValue("passwd")
+// Returns a HanlderFunc that processes login POST form information via the given database handle
+func loginPost(db *sql.DB) http.HandlerFunc {
+	// Actual http request handler with dependency on the `db` database reference
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Request login form data
+		uname := r.FormValue("uname")
+		passwd := r.FormValue("passwd")
 
-	var userid int
-	err := defaultDB.
-		QueryRow("SELECT id FROM users WHERE uname == ? AND passwd == ?", uname, passwd).
-		Scan(&userid)
+		// Check for a db user entry with given data
+		var userid int
+		err := db.
+			QueryRow("SELECT id FROM users WHERE uname == ? AND passwd == ?", uname, passwd).
+			Scan(&userid)
 
-	if err != nil {
-		http.Redirect(w, r, "/auth/login/", http.StatusNotFound)
-		return
+		// No matching user could be found, redirecting back to login form
+		if err != nil {
+			http.Redirect(w, r, "/auth/login/", http.StatusNotFound)
+			return
+		}
+
+		// Store new authentication cookie. Error decoding existing cookie can be ignored since we
+		// are storing a new cookie
+		authCookie, _ := store.Get(r, "auth")
+
+		// Save new auth:status:true cookie
+		authCookie.Values["status"] = true
+		err = authCookie.Save(r, w)
+
+		// Log errors in storing auth cookies for later review
+		if err != nil {
+			log.Println("Saving <auth> cookie failed: %w", err)
+		}
+
+		// Redirect to auth index page to show logged in status
+		http.Redirect(w, r, "/auth/", http.StatusFound)
 	}
-
-	authCookie, _ := store.Get(r, "auth")
-	// Error decoding existing cookie can be ignored since we are storing a new cookie
-
-	// Save new auth:status:true cookie
-	authCookie.Values["status"] = true
-	err = authCookie.Save(r, w)
-
-	// Log errors in storing auth cookies for alter review
-	if err != nil {
-		log.Println("Saving <auth> cookie failed: %w", err)
-	}
-
-	// Redirect to auth page to show logged in status
-	http.Redirect(w, r, "/auth/", http.StatusFound)
 }
 
 func logoutRoute(w http.ResponseWriter, r *http.Request) {}
