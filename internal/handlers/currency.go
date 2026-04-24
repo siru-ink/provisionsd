3b884ba21ca1a358a653bc3c30d05557c819657b
siru <email@siru.ink>
2026-04-24 20:37:58 +0200

    implemented draft of currency post-data database insert

--- internal/handlers/currency.go
+++ internal/handlers/currency.go
@@ -3,13 +3,36 @@ package handlers
 import (
 	"database/sql"
 	"html/template"
+	"log"
 	"net/http"
 
 	"git.siru.ink/siru/provisionsd/internal/templates"
 )
 
 func CreateCurrency(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {}
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Parse the form into data representation
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Bad form data.", http.StatusBadRequest)
+			return
+		}
+
+		// Get form data
+		longname := r.FormValue("currency-longname")
+		shortname := r.FormValue("currency-shortname")
+		symbol := r.FormValue("currency-symbol")
+
+		// Attempt to store data in database
+		_, err = db.Exec("INSERT INTO currencies(longname, shortname, symbol) VALUES ('?', '?', '?')", longname, shortname, symbol)
+		if err != nil {
+			log.Printf("Inserting currency into database failed: %v\n", err)
+			http.Error(w, "Failed storing data in database.", http.StatusInternalServerError)
+		}
+
+		// Redirect on success
+		http.Redirect(w, r, "/currency/", http.StatusFound)
+	}
 }
 
 // Show a pre-populated html form for creating a new currency in the database
